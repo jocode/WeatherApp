9b3aefe72667e64789d01d7fe484ff1a82cf7935
Johan Mosquera <joncamilomoni@gmail.com>
2023-07-14 16:38:40 -0500

    WeatherViewModel and state for the view

---
+++ app/src/main/java/com/crexative/weatherapp/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.crexative.weatherapp.presentation
+
+import com.crexative.weatherapp.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
